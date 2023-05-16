dbe68c7363be3a9a75674d66b3f0478b53ff9dcf
Arturo2711 <132433617+Arturo2711@users.noreply.github.com>
2023-05-16 01:07:47 -0600

    Update CentroComercial.java

--- CentroComercial.java
+++ CentroComercial.java
@@ -16,6 +16,9 @@ public class CentroComercial {
     private int numElevadores;
     private int numEntradas;
     private int numSanitarios;
+    
+    // Atributo - Composicion con TiendaDepartamental
+   private TiendaDepartamental miTiendaDepartamental;
 
     // Metodos
     CentroComercial(){
@@ -53,4 +56,31 @@ public class CentroComercial {
     public void setNumSanitarios(int num){
         numSanitarios = num;
     }
+    
+  // Metodo para composicion con TiendaDepartamental
+  public void tienda_composicion(
+    String nom_tienda,
+    String producto,
+    String nombre_franquicia,
+    int dinero_caja,
+    int num_cajasregistradoras,
+    String telefono,
+    List<Provedor> provedoreLista
+  ) {
+    miTiendaDepartamental =
+      new TiendaDepartamental(
+        nom_tienda,
+        producto,
+        nombre_franquicia,
+        dinero_caja,
+        num_cajasregistradoras,
+        telefono,
+        provedoreLista
+      );
+  }
+
+  // Metodo que utiliza la composicion
+  public void get_lista_provedores_tienda() {
+    miTiendaDepartamental.imprime_provedores();
+  }
 }
